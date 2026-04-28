bded6f3e2e5cd39f72febef7f4c6cf83a8305c45
alexis <kuhlichalexis@gmail.com>
2026-04-28 12:53:54 +0200

    feat: densité par ingrédient + unités personnalisées dans paramètres

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Gestion du cache hors-ligne
 ═══════════════════════════════════════ */
 
-const CACHE_NAME = 'mixtura-v4.1';
+const CACHE_NAME = 'mixtura-v4.2';
 const ASSETS = [
   './',
   './index.html',
